077c61ec7568db2f9dd92a9d5f221083122b25b4
SA Al Walid <saawalid03@gmail.com>
2025-05-29 15:18:21 +0600

    fix: responsive problem solved

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
 
   return (
     <div className="relative w-full bg-gradient-to-t from-blue-900 to-gray-900 h-full">
-      <div className="flex -mt-[5px] flex-col justify-center items-center h-screen text-center relative z-10 space-y-3">
+      <div className="flex -mt-[74px] flex-col justify-center items-center h-screen text-center relative z-10 space-y-3">
         <MotivationalQuote />
         <h3 className="text-5xl font-extrabold text-white drop-shadow-lg pt-6">
           Task Management System
